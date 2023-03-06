946e2ee316348daae6e0058268060ead383fe502
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-03-06 17:32:43 -0500

    add Home section with pic

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,9 @@
 import React from 'react'
 
-const About = () => {
+export default function About () {
   return (
     <section id="about" >
         <div className="about-container">
-        <div id="image-containter">
-            <img src="https://i.imgur.com/rNgGqOQ.jpg?1"/>
-        </div>
         <div id="about-text">
             <h2>Elizabeth (Beth) Hollinger, Full Stack Engineer</h2>
             <p> A physical therapist gone rogue to explore the digital world of code! I am a Full Stack Software Engineer who champions diversity, equity and inclusion 🏳️‍🌈. I became interested in coding during the pandemic. I realized that I enjoyed the challenges of developing and designing mobile and web applications. Since my Software Engineering Immersive, I am even more excited to apply what I have learned into practice diving into projects, volunteer work and Hackathons. In my spare time you'll find me plucking away on my ukulele and getting lost in the forest! </p>
@@ -15,5 +12,3 @@ const About = () => {
     </section>   
   )
 }
-
-export default About
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section id="home">
+        <div>
+            <h1>Hello World!</h1>
+            <div id="image-containter">
+                <img src="https://i.imgur.com/rNgGqOQ.jpg?1"/>
+            </div>
+            <h1>I'm Beth</h1>
+            <h1>A Full Stack Developer</h1>
+        </div>
+    </section>    
+    
+  )
+}
