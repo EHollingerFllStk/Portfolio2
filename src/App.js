e208754a6964f34e9ddeda8fc2f0800f9a363346
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-03-06 17:46:19 -0500

    add footer and nav section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div id="footer">
+        <h6>2023 Copyright Elizabeth Hollinger. All Rights Reserved</h6>
+    </div>
+  )
+}
