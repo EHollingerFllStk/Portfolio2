858b194a9f316554a5cbf8757ab614f42c9e26ed
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-03-06 17:21:52 -0500

    add about page content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section id="about" >
+        <div className="about-container">
+        <div id="image-containter">
+            <img src="https://i.imgur.com/rNgGqOQ.jpg?1"/>
+        </div>
+        <div id="about-text">
+            <h2>Elizabeth (Beth) Hollinger, Full Stack Engineer</h2>
+            <p> A physical therapist gone rogue to explore the digital world of code! I am a Full Stack Software Engineer who champions diversity, equity and inclusion 🏳️‍🌈. I became interested in coding during the pandemic. I realized that I enjoyed the challenges of developing and designing mobile and web applications. Since my Software Engineering Immersive, I am even more excited to apply what I have learned into practice diving into projects, volunteer work and Hackathons. In my spare time you'll find me plucking away on my ukulele and getting lost in the forest! </p>
+        </div>         
+        </div>
+    </section>   
+  )
+}
+
+export default About
